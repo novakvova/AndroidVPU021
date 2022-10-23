9667aaa1323ea30ed8b0f32dc8ba9d594468be33
novakvova <novakvova@gmail>
2022-10-23 13:13:10 +0300

    Begin auth test jwt

--- Web.Pizza/Web.Pizza/Controllers/WeatherForecastController.cs
+++ Web.Pizza/Web.Pizza/Controllers/WeatherForecastController.cs
@@ -23,11 +23,12 @@ namespace Web.Pizza.Controllers
         [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
+            var userName = User.Claims.FirstOrDefault().Value;
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)] + " " + userName
             })
             .ToArray();
         }
